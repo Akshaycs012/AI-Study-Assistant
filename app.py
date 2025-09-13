2ddac135a939e310eb6062b114482e333042f0bb
akshay bhovi <your_github_email@example.com>
2025-09-14 03:04:47 +0530

    push changes for railway

--- app.py
+++ app.py
@@ -201,4 +201,6 @@ def edit_task():
         return f"Failed to edit: {e}", 400
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
+
